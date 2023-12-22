7913a4bde82941ddd02405eef951c1bb55708608
monukmodi <monukr2012@gmail.com>
2023-12-22 13:30:35 +0530

    Added Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <div className="head-top">
+      <div>icon</div>
+      <div>icon</div>
+      <div className="head-user">
+        <div>icon</div>
+        <p>Abhishek Mohanty</p>
+        <div>icon</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
